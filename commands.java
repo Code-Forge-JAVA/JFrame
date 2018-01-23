1cb4b7d4996b4db0b79a85f800fb1670b0eac0bd
Zilvinus-Peciulis <zilvinus.peciulis@gmail.com>
2018-01-23 14:17:59 +0200

    new

--- commands.java
+++ commands.java
@@ -3,3 +3,8 @@
 jButtonOK.setText("Ok MDFK");// Set Text
 jButtonNamesOK.setVisible(false); // show button
 jButtonNamesOK.setEnabled(true); // Enable to react to event (makes gray)
+jTextFieldNames.requestFocus(true);
+jPanel1.requestFocusInWindow(); // Focus on on something else then on  texfield
+
+jTextFieldNames.getText().isEmpty() //
+jTextFieldNames.getText().lenght()
